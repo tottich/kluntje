c1b80815b264059da60262c6605bb2233b67cd89
tottich <tottich@pro-vision.de>
2021-02-21 23:22:55 +0100

    docs(js/utils): fixed doc for util

--- packages/js-utils/src/object-helpers/lib/waitForValue.ts
+++ packages/js-utils/src/object-helpers/lib/waitForValue.ts
@@ -3,19 +3,14 @@ import { getValue } from '..';
 /**
  * waits until a property exists in given object
  * @param {HTMLElement} target
- * @param {string} selector
+ * @param {string} path
  * @param {number} timeout - timeout in milliseconds
  * @param {number} inv - poll interval
  * @returns {Promise<void>}
  * @example
  * waitForValue(window, 'some.api.config', 500).then(() => doSomethingWithAPI());
  */
-export const waitForValue = <T = any>(
-  target: Object = {},
-  path: string,
-  timeout?: number,
-  inv: number = 10,
-): Promise<T> => {
+export const waitForValue = <T = any>(target: any, path: string, timeout?: number, inv: number = 10): Promise<T> => {
   return new Promise((resolve, reject) => {
     const invCheck = setInterval(() => {
       const value = getValue(target, path);
